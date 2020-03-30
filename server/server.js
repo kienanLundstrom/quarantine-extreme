2fb73ddf5af4b129d415c8ba98251be5c2e34cba
Kienan Lundstrom <keen.lundy@gmail.com>
2020-03-30 10:57:57 -0500

    added server up and running

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+/** ---------- MIDDLEWARE ---------- **/
+app.use(bodyParser.json()); // needed for react requests
+app.use(express.static('build'));
+
+/** ---------- ROUTES ---------- **/
+
+/** ---------- START SERVER ---------- **/
+app.listen(port, function () {
+    console.log('Listening on port: ', port);
+});
