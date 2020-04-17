7d1b9f9fc5af057dc754f692f56bbf9969d8ea6d
Kienan Lundstrom <keen.lundy@gmail.com>
2020-04-17 11:41:29 -0500

    added heroku db

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -21,9 +21,9 @@ if (process.env.DATABASE_URL) {
   };
 } else {
   config = {
-    host: 'localhost', // Server hosting the postgres database
+    host: 'ec2-52-87-135-240.compute-1.amazonaws.com', // Server hosting the postgres database
     port: 5432, // env var: PGPORT
-    database: 'quarantineExtreme', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    database: 'd5g69dbrm65b0r', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
