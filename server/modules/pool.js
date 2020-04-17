e9374ef527a55c39b3a6e98f00fb897c2fcbc547
Kienan Lundstrom <keen.lundy@gmail.com>
2020-04-17 11:51:20 -0500

    changing pool to work with heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -21,12 +21,14 @@ if (process.env.DATABASE_URL) {
   };
 } else {
   config = {
-    host: 'ec2-52-87-135-240.compute-1.amazonaws.com', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: 'd5g69dbrm65b0r', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    user: process.env.PG_USER || null, //env var: PGUSER
+    password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
+    host: process.env.DATABASE_SERVER || 'localhost', // Server hosting the postgres database
+    port: process.env.DATABASE_PORT || 5432, //env var: PGPORT
+    database: process.env.DATABASE_NAME || 'quarantineExtreme', //env var: PGDATABASE or the name of your database (e.g. database: process.env.DATABASE_NAME || 'koala_holla',)
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
+};
 }
 
 // this creates the pool that will be shared by all other modules
